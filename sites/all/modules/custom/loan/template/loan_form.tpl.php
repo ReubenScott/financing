704442091cc058207eb4c38c1509abb8c225810d
ReubenScott <ReubenLScott@Hotmail.com>
2015-01-06 16:06:59 +0800

    loan modify

---
+++ sites/all/modules/custom/loan/template/loan_form.tpl.php
@@ -0,0 +1,19 @@
+
+
+
+<div id='error-message' style='color: red;'></div>
+<div>
+<table>
+  <tbody>
+    <tr>
+      <td><?php echo drupal_render($loan_form['loan_platform']); ?></td>
+    </tr>
+    <tr>
+      <td><?php echo drupal_render($loan_form['contract_number']); ?></td>
+    </tr>
+    <tr>
+      <td><?php echo drupal_render($loan_form['item_name']); ?></td>
+    </tr>
+  </tbody>
+</table>
+</div>
